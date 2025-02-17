c2656499faf9db19abeda08557794f23264c67cf
Harshithgowdasm <Harshithgowdasm46@gmail.com>
2025-02-17 16:37:22 +0100

    updated model training

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -1,20 +1,21 @@
 
 import os
-import step_analysis
+from scripts.step_analysis import file_selection
+# from scripts.step_analysis import *
 
-source_folder = r"S:\03_HiWiS\Harshith\ILSC-APP\abc_0000_step_v00\step_09"
-destination_folder = "Selected"
+source_folder = r"S:\03_HiWiS\Harshith\ILSC-APP\3D_STEP_file_analysis\data\Selected_Manually\Combined\Complex"
+# destination_folder = "Simple_selected"
 
 # Create the destination folder inside the source folder
 destination_path = os.path.join(source_folder, destination_folder)
 os.makedirs(destination_path, exist_ok=True)
 
 #call the file selction function 
-result_df = step_analysis.file_selection(source_folder, destination_folder)
+result_df = file_selection(source_folder, destination_folder)
 
-result_folder_path = r"S:\03_HiWiS\Harshith\ILSC-APP\abc_0000_step_v00\Results_new"
+result_folder_path = r"S:\03_HiWiS\Harshith\ILSC-APP\3D_STEP_file_analysis\data\Selected_Manually\Combined\Complex\Complex_selected"
 # Save the CSV inside the destination folder
-csv_path = os.path.join(result_folder_path, 'selected_analysis_09.csv')
+csv_path = os.path.join(result_folder_path, 'Complex_selected_analysis_result_V06.csv')
 result_df.to_csv(csv_path, index=False)
 print("Completed")
 
